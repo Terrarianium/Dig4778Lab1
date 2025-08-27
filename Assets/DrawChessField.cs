794743d353019553701009cf41e56281f0ca8501
William Hamaric <wi652643@ucf.edu>
2025-08-27 14:30:04 -0400

    Update DrawChessField.cs
    Commented DrawChessField

--- Assets/DrawChessField.cs
+++ Assets/DrawChessField.cs
@@ -13,6 +13,7 @@ public class DrawChessField : MonoBehaviour
     {
         Gizmos.color = gridColor;
 
+        // Draws the vertical lines
         for (int x = 0; x <= gridSizeX; x++)
         {
             Vector3 start = new Vector3(x * cellSize, 0, 0);
@@ -20,6 +21,7 @@ public class DrawChessField : MonoBehaviour
             Gizmos.DrawLine(start, end);
         }
         
+        // Draws the horizontal lines
         for (int y = 0; y <= gridSizeY; y++)
         {
             Vector3 start = new Vector3(0, y * cellSize, 0);
